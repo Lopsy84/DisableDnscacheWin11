51e8c4f9221ce0dd19e7770c36691b2b28c6906d
Lopsy84 <122386309+Lopsy84@users.noreply.github.com>
2023-07-19 07:51:39 -0700

    Add files via upload

--- Main.ps1
+++ Main.ps1
@@ -1,19 +1,21 @@
-#Install-Module -Name NtObjectManager -RequiredVersion 1.1.32
-$DnscacheA = "Powershell Set-Service -Name DnsCache -StartupType Auto"
-$DnscacheD = "Powershell Set-Service -Name DnsCache -StartupType Disabled"
+#Install-Module -Name NtObjectManager
+Set-ExecutionPolicy Unrestricted
+$SDDL = "D:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)S:(AU;FA;CCLCSWRPDTLORC;;;WD)"
+$Dnscache = "Powershell Set-Service -Name DnsCache -SecurityDescriptorSddl $SDDL"
 import-module ntobjectmanager
 if ((Get-Service TrustedInstaller).Status -eq 'Stopped') {
     Start-Service -Name TrustedInstaller
 }
 Set-NtTokenPrivilege SeDebugPrivilege
 $ParentProcess = Get-NtProcess -ServiceName TrustedInstaller
-New-Win32Process $DnscacheA -CreationFlags NoWindow -ParentProcess $ParentProcess
+New-Win32Process $Dnscache -CreationFlags NoWindow -ParentProcess $ParentProcess
+Set-Service -Name DnsCache -StartupType Auto
 Start-Sleep -Seconds 20
 if ((Get-Service DnsCache).Status -eq 'Running') {
     Write-Host "DnsCache Running`n"
-    Start-Process -FilePath "wsl"
-    Start-Process -FilePath "wsaclient" -ArgumentList "/launch wsa://com.android.settings" -NoNewWindow
-    New-Win32Process $DnscacheD -CreationFlags NoWindow -ParentProcess $ParentProcess
+    #Start-Process -FilePath "wsl"
+    #Start-Process -FilePath "wsaclient" -ArgumentList "/launch wsa://com.android.settings" -NoNewWindow
+    Set-Service -Name DnsCache -StartupType Disabled
     Start-Sleep -Seconds 5
     $processId = (Get-WmiObject -Class Win32_Service | Where-Object -Property Name -Like *dnscache*).ProcessId
     Write-Host "$processId`n"
